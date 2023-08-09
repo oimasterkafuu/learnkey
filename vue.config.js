ada0e3ce84970659735ef9e708fdf287f0fdea2f
oimasterakioi <oimasterfake@icloud.com>
2023-08-09 16:29:25 +0900

    Fix service worker not updating

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,34 @@ module.exports = defineConfig({
             args[0].title = 'Learn Key';
             return args;
         });
+    },
+    pwa: {
+        name: 'Learn Key',
+        themeColor: '#f3f3f3',
+        iconPaths: {
+            faviconSVG: null,
+            favicon32: 'img/icons/favicon-32x32.png',
+            favicon16: 'img/icons/favicon-16x16.png',
+            appleTouchIcon: 'img/icons/apple-touch-icon-180x180.png',
+            maskIcon: null,
+            msTileImage: null
+        },
+        manifestOptions: {
+            icons: [
+                {
+                    src: './img/icons/android-chrome-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png'
+                },
+                {
+                    src: './img/icons/android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png'
+                }
+            ]
+        },
+        workboxOptions: {
+            skipWaiting: true
+        }
     }
 });
